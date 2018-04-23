0acdffab2d5e1e3c170b6b966726fa337beda2c8
Jake Maschoff <maschoffjake@gmail.com>
2018-04-23 00:04:32 -0600

    Should now be able to read data from GPS module.

--- embedded_system/Lab_6/Src/main.c
+++ embedded_system/Lab_6/Src/main.c
@@ -5,7 +5,7 @@
   ******************************************************************************
   ** This notice applies to any and all portions of this file
   * that are not between comment pairs USER CODE BEGIN and
-  * USER CODE END. Other portions of this file, whether 
+  * USER CODE END. Other portions of this file, whether
   * inserted by the user or by software development tools
   * are owned by their respective copyright owners.
   *
@@ -66,9 +66,26 @@ void SystemClock_Config(void);
 char inputChar;
 int inputFlag;
 
+// Used to store the GPS data that is read in from the module. The messages will never exceed 256 bytes
+// Current index will be used to insert into the appropiated position into the array
+char[257] GPS_msg;
+int current_index;
+
 void USART3_4_IRQHandler() {
 	inputChar = USART3->RDR;
 	inputFlag = 1;
+}
+
+void USART1_IRQHandler() {
+	inputChar = USART1->RDR;
+	GPS_msg[current_index++] = inputChar;
+
+	// If a new line character is reached, then we know that all the data has been read by the module
+	if (inputChar == '\n'){
+		GPS_msg[current_index] = '\0';
+		sendString(GPS_msg);
+		current_index = 0;
+	}
 }
 
 /* USER CODE BEGIN 0 */
@@ -100,94 +117,94 @@ void I2C_Init() {
 		I2C2->TIMINGR |= (1 << 28) | (0x13) | (0xF << 8) | (0x2 << 16) | (0x4 << 20);
 	// Enable the I2C peripheral using the PE bit in CR1 register
   I2C2->CR1 |= 1;
-  
+
   // Set the address to the correct peripheral.
   I2C2->CR2 |= (0x6B << 1);
-  
+
   // Set number of bytes to send to 1
   I2C2->CR2 |= (1 << 16);
-  
-  // Set the start bit 
+
+  // Set the start bit
   I2C2->CR2 |= (1 << 13);
-  
+
   while (!((I2C_ISR_NACKF & I2C2->ISR) | (I2C_ISR_TXIS & I2C2->ISR))){ }
-    
+
   if ((I2C_ISR_NACKF & I2C2->ISR)){
     // BAD
   }
   if ((I2C_ISR_TXIS & I2C2->ISR)){
-    // GOOD 
+    // GOOD
   }
-  
+
   // Send the correct WHO_AM_I information
   I2C2->TXDR = 0x0F;
-  
+
   // Wait for TC flag to set
   while (!(I2C_ISR_TC & I2C2->ISR)){ }
-  
+
   // Set the address to the correct peripheral.
   I2C2->CR2 = (0x6B << 1);
-  
+
   // Set number of bytes to read to 1
   I2C2->CR2 |= (1 << 16);
-  
+
   // Enable the read operation
   I2C2->CR2 |= (1 << 10);
-  
-  // Set the start bit 
+
+  // Set the start bit
   I2C2->CR2 |= (1 << 13);
-  
+
   while (!((I2C_ISR_NACKF & I2C2->ISR) | (I2C_ISR_RXNE & I2C2->ISR))){ }
-  
+
   uint8_t WHO_AM_I = I2C2->RXDR;
-  
+
   // Wait for TC flag to set
   while (!(I2C_ISR_TC & I2C2->ISR)){ }
-  
+
   // Set the address to the correct peripheral.
   I2C2->CR2 = (0x6B << 1);
-  
+
   // Set number of bytes to send to 2
   I2C2->CR2 |= (1 << 17);
-  
+
   // Enable the write operation
   I2C2->CR2 &= ~(1 << 10);
-  
-  // Set the start bit 
+
+  // Set the start bit
   I2C2->CR2 |= (1 << 13);
-  
+
   while (!((I2C_ISR_NACKF & I2C2->ISR) | (I2C_ISR_TXIS & I2C2->ISR))){ }
-  
+
   // Set the address to CTRL_REG1
   I2C2->TXDR = 0x20;
-  
+
   // Wait for TXIS flag to set
   while (!(I2C_ISR_TXIS & I2C2->ISR)){ }
 
   // Set the XEN, YEN, and PEN
   I2C2->TXDR = 0x0B;
-  
+
   // Wait for TC flag to set
   while (!(I2C_ISR_TC & I2C2->ISR)){ }
 }
 
-void USART_Init() {	
+void USART_Init() {
     // Set GPIO pins PC4 and PC5 to AFM.
     GPIOC->MODER |= (1 << 9) | (1 << 11);
     //GPIOB->MODER &= ~((1 << 20) | (1 << 22));
-	
+
 	// Set AFM for PC4 & 5.
 	GPIOC->AFR[0] |= (1 << 20) | (1 << 16);
-	
+
 	// Set the baud rate of USART3
 	USART3->BRR = HAL_RCC_GetHCLKFreq() / 115200;
-	
+
 	// Enable RX and TX in USART3
 	USART3->CR1 |= (1 << 2) | (1 << 3) | (1 << 5);
-	
-	// Enable USART3	
+
+	// Enable USART3
 	USART3->CR1 |= (1 << 0);
-	
+
 	NVIC_EnableIRQ(USART3_4_IRQn);
 }
 
@@ -203,24 +220,49 @@ void LED_Init() {
 
 void SPI_Init() {
 	// SET UP PINS
-	
+
 	// Setting pins PB3 to PB5 (SPI1_SCK, SPI1_MISO, & SPI1_MOSI respectively) on Alternate function Mode
 	GPIOB->MODER |= (1 << 7) | (1 << 9) | (1 << 11);
     // Set pin PA15 to alternate function mode for SPI1_NSS
     GPIOA->MODER |= (1 << 31);
-	
-	// SPI Enabled 
+
+	// SPI Enabled
 	SPI1->CR1 |= (1 << 6);
-	
+
 	// set up SPI for full-duplex mode (bit 10 = 0)
 	SPI1->CR1 &= ~(1 << 10);
-	
+
 	// CPOL 0 and CPHA 0, first rising edge
 	SPI1->CR1 &= ~((1 << 1) | (1 << 0));
 
 	// Set Master Config.
 	SPI1->CR1 |= (1 << 2);
 }
+
+/*
+*	Used for setting up the UART that is used by the GPS module
+*/
+void UART_GPS_Init(){
+	// Set PA9 and PA10 to AFM (5V tolerant), use AF1 for USART1
+	GPIOA->MODER |= (1 << 19) | (1 << 21);
+	GPIOA->AFR[1] |= (1 << 4) | (1 << 8);
+
+	// Set BAUD rate to 9600 bits/sec
+	USART1->BRR = HAL_RCC_GetHCLKFreq() / 9600;
+
+	// Enable RX and TX in USART1 and disable the interrupt flag to start.
+	USART1->CR1 |= (1 << 2) | (1 << 3) | (1 << 5);
+
+	// Now turn on the USART
+	USART1->CR1 |= 1;
+
+	// Enable the interrupt for this USART
+	NVIC_EnableIRQ(USART1_IRQn);
+
+	// Set the current index to 0, since we are reading in fresh data
+	current_index = 0;
+}
+
 
 /* USER CODE END 0 */
 
@@ -233,17 +275,19 @@ int main(void)
 {
 	HAL_Init();
   SystemClock_Config();
-  
+
 	// Enable the RCC for USART
-    RCC->APB1ENR |= RCC_APB1ENR_USART3EN;
+  RCC->APB1ENR |= RCC_APB1ENR_USART3EN;
+	RCC->APB1ENR |= RCC_APB1ENR_USART1EN;
 	RCC->AHBENR |= RCC_AHBENR_GPIOCEN;
-	RCC->AHBENR |= RCC_AHBENR_GPIOBEN;	
-	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;	
+	RCC->AHBENR |= RCC_AHBENR_GPIOBEN;
+	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
 	RCC->APB1ENR |= RCC_APB1ENR_I2C2EN;
     // RCC->APB1ENR |= RCC_APB1ENR_I2C2EN; likely need a line like this to set up a clock for SPI
 
 	LED_Init();
 	USART_Init();
+	UART_GPS_Init();
 
 	GPIOB->MODER |= (1 << 23) | (1 << 27) | (1 << 28);
 	GPIOC->MODER |= 1;
@@ -251,140 +295,140 @@ int main(void)
 	GPIOB->AFR[1] |= (1 << 12) | (1 << 20) | (1 << 22);
 	GPIOB->ODR |= (1 << 14);
 	GPIOC->ODR |= 1;
-	
+
 	I2C_Init();
 	SPI_Init();
-  
+
   int16_t X_axis;
   int16_t Y_axis;
-	
+
 	while(1) {
 		 // Wait 100 ms
 		HAL_Delay(100);
 
     // Set the address to the correct peripheral.
     I2C2->CR2 = (0x6B << 1);
-  
+
     // Set number of bytes to send to 1
     I2C2->CR2 |= (1 << 16);
-  
+
     // Enable the write operation
     I2C2->CR2 &= ~(1 << 10);
-    
-    // Set the start bit 
-    I2C2->CR2 |= (1 << 13);  
-    
+
+    // Set the start bit
+    I2C2->CR2 |= (1 << 13);
+
     while (!((I2C_ISR_NACKF & I2C2->ISR) | (I2C_ISR_TXIS & I2C2->ISR))){ }
-    
+
     if ((I2C_ISR_NACKF & I2C2->ISR)){
       // BAD
     }
     if ((I2C_ISR_TXIS & I2C2->ISR)){
-      // GOOD 
+      // GOOD
     }
-  
+
     // Set the address to CTRL_REG1
     I2C2->TXDR = 0xA8;
-    
+
     // Wait for TC flag to set
     while (!(I2C_ISR_TC & I2C2->ISR)){ }
-    
+
     // Set the address to the correct peripheral.
     I2C2->CR2 = (0x6B << 1);
-  
+
     // Set number of bytes to read to 2
     I2C2->CR2 |= (2 << 16);
-  
+
     // Enable the read operation
     I2C2->CR2 |= (1 << 10);
-  
-    // Set the start bit 
+
+    // Set the start bit
     I2C2->CR2 |= (1 << 13);
-  
+
     while (!((I2C_ISR_NACKF & I2C2->ISR) | (I2C_ISR_RXNE & I2C2->ISR))){ }
-  
+
     if ((I2C_ISR_NACKF & I2C2->ISR)){
       // BAD
     }
     if ((I2C_ISR_RXNE & I2C2->ISR)){
-      // GOOD 
+      // GOOD
     }
-    
+
     X_axis = I2C2->RXDR;
-    
+
     while (!(I2C_ISR_RXNE & I2C2->ISR)){ }
-    
+
     X_axis |= I2C2->RXDR << 8;
-    
+
       // Wait for TC flag to set
     while (!(I2C_ISR_TC & I2C2->ISR)){ }
-    
 
-    
-    
+
+
+
     /*
       Y DATA!
-    
+
     */
-    
-    
+
+
     // Set the address to the correct peripheral.
     I2C2->CR2 = (0x6B << 1);
-  
+
     // Set number of bytes to send to 1
     I2C2->CR2 |= (1 << 16);
-  
+
     // Enable the write operation
     I2C2->CR2 &= ~(1 << 10);
-    
-    // Set the start bit 
-    I2C2->CR2 |= (1 << 13);  
-    
+
+    // Set the start bit
+    I2C2->CR2 |= (1 << 13);
+
     while (!((I2C_ISR_NACKF & I2C2->ISR) | (I2C_ISR_TXIS & I2C2->ISR))){ }
-    
+
     if ((I2C_ISR_NACKF & I2C2->ISR)){
       // BAD
     }
     if ((I2C_ISR_TXIS & I2C2->ISR)){
-      // GOOD 
+      // GOOD
     }
-  
+
     // Set the address to CTRL_REG1
     I2C2->TXDR = 0xAA;
-    
+
     // Wait for TC flag to set
     while (!(I2C_ISR_TC & I2C2->ISR)){ }
-    
+
     // Set the address to the correct peripheral.
     I2C2->CR2 = (0x6B << 1);
-  
+
     // Set number of bytes to read to 2
     I2C2->CR2 |= (2 << 16);
-  
+
     // Enable the read operation
     I2C2->CR2 |= (1 << 10);
-  
-    // Set the start bit 
+
+    // Set the start bit
     I2C2->CR2 |= (1 << 13);
-  
+
     while (!((I2C_ISR_NACKF & I2C2->ISR) | (I2C_ISR_RXNE & I2C2->ISR))){ }
-  
+
     if ((I2C_ISR_NACKF & I2C2->ISR)){
       // BAD
     }
     if ((I2C_ISR_RXNE & I2C2->ISR)){
-      // GOOD 
+      // GOOD
     }
-    
+
     Y_axis = I2C2->RXDR;
-    
+
     while (!(I2C_ISR_RXNE & I2C2->ISR)){ }
-    
+
     Y_axis |= I2C2->RXDR << 8;
-    
+
       // Wait for TC flag to set
     while (!(I2C_ISR_TC & I2C2->ISR)){ }
-		
+
 		if (X_axis > 5000) {
 			sendString("RIGHT");
 			GPIOC->ODR |= (1 << GREEN);
@@ -394,7 +438,7 @@ int main(void)
 			GPIOC->ODR |= (1 << ORANGE);
 			GPIOC->ODR &= ~(1 << GREEN);
 		}
-		
+
 		if (Y_axis > 5000) {
 			sendString("TOP");
 			GPIOC->ODR |= (1 << RED);
@@ -405,7 +449,7 @@ int main(void)
 			GPIOC->ODR &= ~(1 << RED);
 		}
 	}
-	
+
 }
 
 /**
@@ -418,7 +462,7 @@ void SystemClock_Config(void)
   RCC_OscInitTypeDef RCC_OscInitStruct;
   RCC_ClkInitTypeDef RCC_ClkInitStruct;
 
-    /**Initializes the CPU, AHB and APB busses clocks 
+    /**Initializes the CPU, AHB and APB busses clocks
     */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
@@ -429,7 +473,7 @@ void SystemClock_Config(void)
     _Error_Handler(__FILE__, __LINE__);
   }
 
-    /**Initializes the CPU, AHB and APB busses clocks 
+    /**Initializes the CPU, AHB and APB busses clocks
     */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1;
@@ -442,11 +486,11 @@ void SystemClock_Config(void)
     _Error_Handler(__FILE__, __LINE__);
   }
 
-    /**Configure the Systick interrupt time 
+    /**Configure the Systick interrupt time
     */
   HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
 
-    /**Configure the Systick 
+    /**Configure the Systick
     */
   HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
 
@@ -483,7 +527,7 @@ void _Error_Handler(char *file, int line)
   * @retval None
   */
 void assert_failed(uint8_t* file, uint32_t line)
-{ 
+{
   /* USER CODE BEGIN 6 */
   /* User can add his own implementation to report the file name and line number,
      tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
